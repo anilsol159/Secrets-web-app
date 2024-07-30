ce52d224c9521b8c70b77c554efd506306fb5b48
Anil Solanki <coderanil0@gmail.com>
2024-07-30 09:47:22 +0530

    database to environment variables

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const saltRounds = 10;
 env.config();
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "Raaz1",
-  password: "AnilKeRaaz",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 db.connect()
 
